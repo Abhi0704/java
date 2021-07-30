5cd8738af80a0cde4735dcbee125feb389fb0658
Abhi0704 <61460316+Abhi0704@users.noreply.github.com>
2021-07-30 20:17:38 +0530

    Logical questions

---
+++ DecimalNumber.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class DecimalNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a Decimal number");
+        double decimalNumber = sc.nextDouble();
+
+        String doubleString = String.valueOf(decimalNumber);
+        int indexOfDecimal = doubleString.indexOf(".");
+
+
+        System.out.println("Integer Part: " + doubleString.substring(0, indexOfDecimal));
+        System.out.println("Decimal Part: " + doubleString.substring(indexOfDecimal+1));
+
+        if (doubleString.substring(indexOfDecimal + 1).equals(doubleString.substring(0, indexOfDecimal))) {
+
+            System.out.println("Equal");
+        } else {
+            System.out.println("Not Equal");
+        }
+    }
+}
